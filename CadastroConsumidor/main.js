96288ac7ed57b39a03211dba8cc06d6b4e1ae975
Douglas <douglasrosso10@gmail.com>
2023-02-16 13:53:53 -0300

    fix master

--- CadastroConsumidor/main.js
+++ CadastroConsumidor/main.js
@@ -1,44 +1,41 @@
 var options = {
-    onKeyPress: function (cpf, ev, el, op) {
-        var masks = ['000.000.000-000', '00.000.000/0000-00'];
-        $('.cpfcnpj').mask((cpf.length > 14) ? masks[1] : masks[0], op);
-    }
-}
-$('.cpfcnpj').length > 11 ? $('.cpfcnpj').mask('00.000.000/0000-00', options) : $('.cpfcnpj').mask('000.000.000-00#', options);
+  onKeyPress: function (cpf, ev, el, op) {
+    var masks = ["000.000.000-000", "00.000.000/0000-00"];
+    $(".cpfcnpj").mask(cpf.length > 14 ? masks[1] : masks[0], op);
+  },
+};
+$(".cpfcnpj").length > 11
+  ? $(".cpfcnpj").mask("00.000.000/0000-00", options)
+  : $(".cpfcnpj").mask("000.000.000-00#", options);
 
-let nome = document.getElementById("nome")
-let cpfcnpj = document.getElementById("cpfcnpj")
-let telefone = document.getElementById("telefone")
-let telefone2 = document.getElementById("telefone2")
+let nome = document.getElementById("nome");
+let cpfcnpj = document.getElementById("cpfcnpj");
+let telefone = document.getElementById("telefone");
+let telefone2 = document.getElementById("telefone2");
 
 function postUser() {
-  
-    fetch(`https://localhost:7230/Consumidor`, {
-    method: 'POST',
+  fetch(`https://localhost:7230/Consumidor`, {
+    method: "POST",
     headers: {
-      "Accept": "*//*",
+      Accept: "*//*",
       "content-type": "application/json",
-
     },
     body: JSON.stringify({
-        Nome_Consumidor: nome.value,
-        Doc_Consumidor: cpfcnpj.value,
-        Telefone1: telefone.value,
-        Telefone2: telefone2.value
-    })
-  })
-  .then(function (response) {
+      Nome_Consumidor: nome.value,
+      Doc_Consumidor: cpfcnpj.value,
+      Telefone1: telefone.value,
+      Telefone2: telefone2.value,
+    }),
+  }).then(function (response) {
     if (response.status == 201) {
       alert("Consumidor cadastrado");
-
+    } else if (response.status == 409) {
+      alert("Consumidor Ja existe no banco de dados");
     }
-    else if(response.status == 409){
-        alert("Consumidor Ja existe no banco de dados");
-    }
-  })
+  });
 }
 var button = document.querySelector("button");
 button.addEventListener("click", function (event) {
   event.preventDefault();
-  postUser()
+  postUser();
 });
