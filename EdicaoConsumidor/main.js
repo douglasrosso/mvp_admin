61017415206afa4fb32067b4b3ce077db619bf5f
Douglas <douglasrosso10@gmail.com>
2023-02-22 13:53:06 -0300

    fix merge

--- EdicaoConsumidor/main.js
+++ EdicaoConsumidor/main.js
@@ -1,86 +1,77 @@
-var options = {
-  onKeyPress: function (cpf, ev, el, op) {
-    var masks = ["000.000.000-000", "00.000.000/0000-00"];
-    $(".cpfcnpj").mask(cpf.length > 14 ? masks[1] : masks[0], op);
-  },
-};
-$(".cpfcnpj").length > 11
-  ? $(".cpfcnpj").mask("00.000.000/0000-00", options)
-  : $(".cpfcnpj").mask("000.000.000-00#", options);
+let nome = document.getElementById("nome")
+let cpfcnpj = document.getElementById("cpfcnpj")
+let telefone = document.getElementById("telefone")
+let telefone2 = document.getElementById("telefone2")
 
-let documento = document.getElementById("cpfcnpj");
+let documento
+
 let codConsumidor;
-let documentoDigitado;
-let cepDigitado;
-let btn = document.getElementById("btn");
 
-function TiraSinais() {
-  if (documento.value.length == 14) {
-    documentoDigitado =
-      documento.value.slice(0, 3) +
-      documento.value.slice(4, 7) +
-      documento.value.slice(8, 11) +
-      documento.value.slice(12, 14);
-  } else {
-    documentoDigitado =
-      documento.value.slice(0, 2) +
-      documento.value.slice(3, 6) +
-      documento.value.slice(7, 10) +
-      documento.value.slice(11, 15) +
-      documento.value.slice(16, 18);
-  }
-  cepDigitado =
-    cep.value.slice(0, 2) + cep.value.slice(3, 6) + cep.value.slice(7, 10);
-}
 
-function getConsumidor() {
-  TiraSinais();
-  fetch(`https://localhost:7230/Consumidor/Documento/${documentoDigitado}`)
-    .then((response) => response.json())
-    .then((data) => {
-      codConsumidor = data.cod_Consumidor;
-    })
-    .catch((error) => console.error(error));
-}
+const Myselect = document.getElementById("my-select");
 
-// busca cod pelo documento
-function postUser() {
-  fetch(`https://localhost:7230/UC`, {
-    method: "POST",
-    headers: {
-      Accept: "*//*",
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      cod_Consumidor: codConsumidor,
-      num_medidor: numerodomedidor.value,
-      num_casa: numeroCasa.value,
-      cep: cepDigitado,
-      logradouro: logradoro.value,
-      Bairro: bairro.value,
-      complemento: complemento.value,
-    }),
-  }).then(function (response) {
-    if (response.status == 201) {
-      alert("Consumidor Inserido");
-    } else {
-      alert("Houve um problema, no envio da requesição");
-    }
+
+function getConsumidores(){
+  fetch('https://localhost:7230/Consumidor')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+        console.log(data)
+      let option = document.createElement('option');
+      console.log(option)
+      option.textContent = item.doc_consumidor
+      option.value = item.nome_Consumidor;
+
+      nome.value = item.nome_Consumidor
+      telefone.value = item.telefone1
+      telefone2.value = item.telefone2
+
+      codConsumidor = item.cod_Consumidor
+      documento = item.doc_consumidor
+      Myselect.appendChild(option);
+    });
+
   });
 }
 
-function LimpaCampos() {
-  documento.value = "";
-  cep.value = "";
-  logradoro.value = "";
-  numeroCasa.value = "";
-  complemento.value = "";
-  numerodomedidor.value = "";
-  bairro.value = "";
-}
 
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  postUser();
-  LimpaCampos();
+var input = document.getElementById("myinput")
+input.addEventListener("keydown", function(){
+  if(input.value.length==5){
+    getConsumidores()
+    
+  }
+  
+})
+
+
+function putUser() {
+
+  fetch(`https://localhost:7230/Consumidor/${codConsumidor}`, {
+  method: 'PUT',
+  headers: {
+    "Accept": "*//*",
+    "content-type": "application/json",
+
+  },
+  body: JSON.stringify({
+      Nome_Consumidor: nome.value,
+      Doc_Consumidor: documento,
+      Telefone1: telefone.value,
+      Telefone2: telefone2.value
+  })
+})
+.then(function (response) {
+  if (response.status == 204) {
+    alert("Consumidor cadastrado");
+  }
+  else if(response.status == 409){
+      alert("Consumidor Ja existe no banco de dados");
+  }
+})
+}
+var button = document.querySelector("button");
+button.addEventListener("click", function (event) {
+event.preventDefault();
+putUser()
 });
