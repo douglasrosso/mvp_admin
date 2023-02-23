15e35ebc14e0737828e59459cb378f6ab7413d21
Douglas <douglasrosso10@gmail.com>
2023-02-23 15:14:44 -0300

    last commit

--- CadastroConsumidor/main.js
+++ CadastroConsumidor/main.js
@@ -1,17 +1,18 @@
 var options = {
   onKeyPress: function (cpf, ev, el, op) {
-      var masks = ['000.000.000-000', '00.000.000/0000-00'];
-      $('.cpfcnpj').mask((cpf.length > 14) ? masks[1] : masks[0], op);
-  }
-}
-$('.cpfcnpj').length > 11 ? $('.cpfcnpj').mask('00.000.000/0000-00', options) : $('.cpfcnpj').mask('000.000.000-00#', options);
+    var masks = ["000.000.000-000", "00.000.000/0000-00"];
+    $(".cpfcnpj").mask(cpf.length > 14 ? masks[1] : masks[0], op);
+  },
+};
+$(".cpfcnpj").length > 11
+  ? $(".cpfcnpj").mask("00.000.000/0000-00", options)
+  : $(".cpfcnpj").mask("000.000.000-00#", options);
 
-let nome = document.getElementById("nome")
-let cpfcnpj = document.getElementById("cpfcnpj")
-let telefone = document.getElementById("telefone")
-let telefone2 = document.getElementById("telefone2")
-let cpfcnpjDigitado
-
+let nome = document.getElementById("nome");
+let cpfcnpj = document.getElementById("cpfcnpj");
+let telefone = document.getElementById("telefone");
+let telefone2 = document.getElementById("telefone2");
+let cpfcnpjDigitado;
 
 function TiraSinais() {
   if (cpfcnpj.value.length == 14) {
@@ -31,43 +32,60 @@ function TiraSinais() {
 }
 
 function postUser() {
-
-  fetch(`https://localhost:7230/Consumidor`, {
-  method: 'POST',
-  headers: {
-    "Accept": "*//*",
-    "content-type": "application/json",
-
-  },
-  body: JSON.stringify({
-      Nome_Consumidor: nome.value,
-      Doc_Consumidor: cpfcnpjDigitado,
-      Telefone1: telefone.value,
-      Telefone2: telefone2.value
-  })
-})
-.then(function (response) {
-  if (response.status == 201) {
-    alert("Consumidor cadastrado");
-    LimpaCampos()
+  if (telefone2.value == "") {
+    fetch(`https://localhost:7230/Consumidor`, {
+      method: "POST",
+      headers: {
+        Accept: "*//*",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        Nome_Consumidor: nome.value,
+        Doc_Consumidor: cpfcnpjDigitado,
+        Telefone1: telefone.value,
+      }),
+    }).then(function (response) {
+      if (response.status == 201) {
+        alert("Consumidor cadastrado");
+        LimpaCampos();
+      } else if (response.status == 409) {
+        alert("Consumidor Ja existe no banco de dados");
+      }
+    });
+  } else {
+    fetch(`https://localhost:7230/Consumidor`, {
+      method: "POST",
+      headers: {
+        Accept: "*//*",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        Nome_Consumidor: nome.value,
+        Doc_Consumidor: cpfcnpjDigitado,
+        Telefone1: telefone.value,
+        Telefone2: telefone2.value,
+      }),
+    }).then(function (response) {
+      if (response.status == 201) {
+        alert("Consumidor cadastrado");
+        LimpaCampos();
+      } else if (response.status == 409) {
+        alert("Consumidor Ja existe no banco de dados");
+      }
+    });
   }
-  else if(response.status == 409){
-      alert("Consumidor Ja existe no banco de dados");
-  }
-})
 }
 
 function LimpaCampos() {
   nome.value = "";
   cpfcnpj.value = "";
   telefone.value = "";
-  telefone2.value= "";
-
+  telefone2.value = "";
 }
 
 var button = document.querySelector("button");
 button.addEventListener("click", function (event) {
-event.preventDefault();
-TiraSinais()
-postUser()
+  event.preventDefault();
+  TiraSinais();
+  postUser();
 });
